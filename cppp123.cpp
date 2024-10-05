63c93278006e11890f2ad7d6ecfece92e0f7057d
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-10-05 12:39:12 +0700

    Add files via upload

---
+++ cppp123.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(){
+    
+ 	
+    printf("em ten là PHAM TIEN HUNG\n");  
+ 	printf("ma sinh vien là b24dtcn346\n");
+ 	printf("hien tai dang hoc tap tai HOC VIEN CONG NGHE BUU CHINH VIEN THONG\n");
+	return 0;
+ }
